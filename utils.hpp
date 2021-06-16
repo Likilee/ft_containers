00911330078182ca4d8e1af3d8b63e1fe203336f
Likilee <gnsdlrl@daum.net>
2021-06-16 14:40:43 +0900

    Fix distance

--- utils.hpp
+++ utils.hpp
@@ -5,53 +5,29 @@
 
 namespace ft
 {
-// 1. Iterator Compare Utility function
-template <class InputIterator1, class InputIterator2>
-bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1,
-							InputIterator2 first2, InputIterator2 last2)
-{
-	while (first1 != last1)
-	{
-		if (first2 == last2 || *first2 < *first1)
-			return (false);
-		else if (*first1 < *first2)
-			return (true);
-		++first1;
-		++first2;
-	}
-	return (first2 != last2);
-}
-
-template <class InputIterator1, class InputIterator2>
-bool equal(InputIterator1 first1, InputIterator1 last1,
-			InputIterator2 first2)
-{
-	while (first1!=last1)
-	{
-		if (!(*first1 == *first2))	// or: if (!pred(*first1,*first2)), for version 2
-			return (false);
-		++first1;
-		++first2;
-	}
-	return (true);
-}
-
-template <typename InputItr>
-typename InputItr::difference_type distance(InputItr first, InputItr last)
-{
-	typename InputItr::difference_type dist = 0;
-
-	while (first++ != last)
-		++dist;
-	return (dist);
-}
-
-template <typename Pointer>
-size_t distance(Pointer first, Pointer last)
-{
-	return (last - first);
-}
 // 2. For template overloading
+template <bool B, class T = void>
+struct enable_if;
+
+template <class T>
+struct enable_if<true, T>
+{
+	typedef T type;
+	typedef int yes;
+};
+
+template <typename T, typename U>
+struct is_same
+{
+	static const bool value = false;
+};
+
+template <typename T>
+struct is_same<T,T>
+{
+	static const bool value = true;
+};
+
 // is_pointer
 template <typename T>
 struct remove_const
@@ -101,28 +77,6 @@ bool is_pointer(const T&)
 	return is_pointer_type<T>::value;
 }
 
-template <bool B, class T = void>
-struct enable_if;
-
-template <class T>
-struct enable_if<true, T>
-{
-	typedef T type;
-	typedef int yes;
-};
-
-template <typename T, typename U>
-struct is_same
-{
-	static const bool value = false;
-};
-
-template <typename T>
-struct is_same<T,T>
-{
-	static const bool value = true;
-};
-
 template <typename T, typename Enable = void>
 struct is_iter
 {
@@ -141,5 +95,55 @@ struct is_iter<T, typename ft::enable_if<ft::is_pointer_type<T>::value>::type>
 	static const bool value = true;
 };
 
+// 1. Iterator Compare Utility function
+template <class InputIterator1, class InputIterator2>
+bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1,
+							InputIterator2 first2, InputIterator2 last2)
+{
+	while (first1 != last1)
+	{
+		if (first2 == last2 || *first2 < *first1)
+			return (false);
+		else if (*first1 < *first2)
+			return (true);
+		++first1;
+		++first2;
+	}
+	return (first2 != last2);
+}
+
+template <class InputIterator1, class InputIterator2>
+bool equal(InputIterator1 first1, InputIterator1 last1,
+			InputIterator2 first2)
+{
+	while (first1!=last1)
+	{
+		if (!(*first1 == *first2))	// or: if (!pred(*first1,*first2)), for version 2
+			return (false);
+		++first1;
+		++first2;
+	}
+	return (true);
+}
+
+// template <typename InputItr>
+// typename InputItr::difference_type distance(InputItr first, InputItr last)
+// {
+// 	typename InputItr::difference_type dist = 0;
+
+// 	while (first++ != last)
+// 		++dist;
+// 	return (dist);
+// }
+
+template <typename Pointer>
+size_t distance(Pointer first, Pointer last)
+{
+	size_t dist = 0;
+
+	while (first++ != last)
+		++dist;
+	return (dist);
+}
 }
 #endif
