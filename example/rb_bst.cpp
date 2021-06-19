1d51f20264cd6ca72311f42387d7b6491816ef97
Likilee <gnsdlrl@daum.net>
2021-06-19 16:03:21 +0900

    Add comment about rbtree insert algorithm

--- example/rb_bst.cpp
+++ example/rb_bst.cpp
@@ -348,7 +348,36 @@ public:
 		else
 			parent->right = current;
 		// **** 여기부터 RBTREE 기능 추가
+// 		Algorithm:
+// Let x be the newly inserted node.
+
+// Perform standard BST insertion and make the colour of newly inserted nodes as RED.
+// If x is the root, change the colour of x as BLACK (Black height of complete tree increases by 1).
+// Do the following if the color of x’s parent is not BLACK and x is not the root.
+// a) If x’s uncle is RED (Grandparent must have been black from property 4)
+// (i) Change the colour of parent and uncle as BLACK.
+// (ii) Colour of a grandparent as RED.
+// (iii) Change x = x’s grandparent, repeat steps 2 and 3 for new x.
+// b) If x’s uncle is BLACK, then there can be four configurations for x, x’s parent (p) and x’s grandparent (g) (This is similar to AVL Tree)
+// (i) Left Left Case (p is left child of g and x is left child of p)
+// (ii) Left Right Case (p is left child of g and x is the right child of p)
+// (iii) Right Right Case (Mirror of case i)
+// (iv) Right Left Case (Mirror of case ii)
+
+
 		// 장애발생! : 신규노드와 부모가 연속으로 red 일 경우!
+		if (redred())
+		{
+			if(uncle is black)
+				rotate()
+			else if (uncle is red)
+				color_change();
+		}
+
+		rotate() // 삽입, 부모, 조부모를 세트로 흔드렁 제껴~
+		{
+
+		}
 		//마지막에 Root color Black으로 바꿔주기
 		if (this->root->color == RED)
 			this->root->color = BLACK;
