1975a6983f2f18e61bbd5db37f644587635f5521
Likilee <gnsdlrl@daum.net>
2021-06-22 08:58:16 +0900

    Devide source code

---
+++ example/rbnode.hpp
@@ -0,0 +1,120 @@
+#ifndef RBNODE_HPP
+# define RBNODE_HPP
+
+namespace ft
+{
+
+enum Color
+{
+	RED,
+	BLACK
+};
+
+template <typename T>
+class rb_node
+{
+public:
+	T *key;
+	rb_node *left;
+	rb_node *right;
+	rb_node *parent;
+	Color color;
+
+	rb_node() : key(NULL), left(NULL), right(NULL), parent(NULL), color(BLACK) {}; // nil_node
+	rb_node(const T& val) : left(NULL), right(NULL), parent(NULL), color(RED)
+	{
+		this->key = new T(val);
+	}
+
+	~rb_node()
+	{
+		delete key;
+	}
+
+	T& getData()
+	{
+		return (*this->key);
+	}
+
+	T& getColor()
+	{
+		return (this->color);
+	}
+
+	rb_node *getLeft()
+	{
+		return (this->left);
+	}
+
+	rb_node *getRight()
+	{
+		return (this->right);
+	}
+
+	rb_node *getParent()
+	{
+		return (this->parent);
+	}
+
+	bool empty()
+	{
+		if (this->key == NULL)
+			return (true);
+		return (false);
+	}
+
+	bool is_root()
+	{
+		if (this->parent->empty())
+			return (true);
+		return (false);
+	}
+
+	bool is_left()
+	{
+		if (this->parent->left == this)
+			return (true);
+		else
+			return (false);
+	}
+
+	bool is_right()
+	{
+		if (this->parent->right == this)
+			return (true);
+		else
+			return (false);
+	}
+
+	bool is_leaf()
+	{
+		if (this->left->empty() && this->right->empty())
+			return (true);
+		return (false);
+	}
+
+	bool has_one_child()
+	{
+		if (!is_leaf() && (this->left->empty() || this->right->empty()))
+			return (true);
+		return (false);
+	}
+
+	bool has_two_child()
+	{
+		if (!this->left->empty() && !this->right->empty())
+			return (true);
+		return (false);
+	}
+
+	rb_node *sibling()
+	{
+		if (this == this->parent->left)
+			return (this->parent->right);
+		else
+			return (this->parent->left);
+	}
+};
+}
+
+#endif
---
+++ example/rbtest.cpp
@@ -0,0 +1,61 @@
+# include "print_tree.hpp"
+# include "rbtree.hpp"
+
+int main()
+{
+	ft::rbtree<int> tree;
+
+	// tree.insert(3);
+	// tree.insert(5);
+	// tree.insert(8);
+	// tree.insert(7);
+	// tree.insert(9);
+	// tree.insert(10);
+	// tree.insert(13);
+	// tree.insert(12);
+	// tree.insert(11);
+	// tree.insert(4);
+	// tree.insert(19);
+	// tree.print();
+	// tree.erase_rbt(8);
+	// // tree.erase_rbt(5);
+	// tree.print();
+	// tree.clear();
+
+
+// TESTER
+	srand(clock());
+	ft::rbtree<int> rbtree;
+	//Insert test
+	std::cout << "*====== INSERT TEST ======*" << std::endl;
+
+	for (int i = 0; i < 1000; ++i)
+		rbtree.insert(rand() % 800);
+	rbtree.print();
+	std::cout << std::endl; //end Insert test
+
+	//Erase test
+	std::cout << "*====== ERASE TEST ======*" << std::endl;
+	for (int i = 0; i < 1000; ++i)
+	{
+		rbtree.erase_rbt(rand() % 800);
+		// rbtree.print();
+		rbtree.check_traversal();
+	}
+	rbtree.print();
+	std::cout << std::endl; //end Erase test
+
+	//Clear test
+	std::cout << "*====== CLEAR TEST ======*" << std::endl;
+	rbtree.clear();
+	rbtree.print();
+
+	std::cout << std::endl; //end Clear test
+
+	//Insert after clear test
+	std::cout << "*====== INSERT AFTER CLEAR TEST ======*" << std::endl;
+	rbtree.insert(1004);
+	rbtree.print();
+
+	return (0);
+}
